443a3900506802fba445406b179fb6794131684a
Preeti Verma <69456153+vermapreeti392@users.noreply.github.com>
2023-03-24 19:12:11 +0530

    .

--- Backend/connection/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {db_url} = require('../keys');
-async function getConnection(){
-    await mongoose.connect(db_url)
-    .then(()=>console.log("database connected successfully"))
-    .catch(err=>console.log(err));
-}
-
-module.exports = getConnection;
--- Backend/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports={
-    db_url:"mongodb+srv://preeti_123:preeti123@cluster2.bowqeft.mongodb.net/Employee"
-}
